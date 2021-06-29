001a2b1468d78a711226073f043dd8de446f026e
trigg <triggerhapp@gmail.com>
2021-06-29 21:38:05 +0100

    - Fix #76

--- plugin/webclient.js
+++ plugin/webclient.js
@@ -3,7 +3,9 @@ const fs = require('fs');
 const express = require('express');
 
 // Get client
-shell.exec('git clone https://github.com/trigg/Rebuttal-Client.git client');
+if (!fs.existsSync('client')) {
+    shell.exec('git clone https://github.com/trigg/Rebuttal-Client.git client');
+}
 // Update it
 shell.cd('client');
 shell.exec('git pull');
